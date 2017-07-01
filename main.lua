ecf0d62a759db254b538f671a96243e13c8f63a9
twentytwoo <may.tt@protonmail.ch>
2017-07-01 13:44:58 +0100

    To hell with OOP

--- main.lua
+++ main.lua
@@ -1,60 +1,32 @@
+require 'sayIt'
+
 function love.load()
-	sayIt_Init()
+	sayIt.Init()
 end
 
-function love.update( dt )
-
-end
-
-function love.draw( )
-	love.graphics.setBackgroundColor( 100, 100, 100  )
-	sayIt_New({"Test", "Test no. two", "And three :)" }, 100, 10, 0, "box")
-end
-
-
-function love.keyreleased(key)
-	sayIt_Handler(key)
-end
-
-function sayIt_New(message, x, y, size, msgType, effect )
-	local width = love.graphics.getWidth()
-	local height = love.graphics.getHeight()
-
-	-- Config
-	local padding = 10
-	local boxHeight = 150
-	local msgBoxPosX = 0+padding
-	local msgBoxPosY = height-padding
-	local msgBoxBg = { 255, 255, 255, 255 } -- RGBA
-	local msgFontColor = { 0, 0, 0, 255}
-	if showMessage then
-		if message[count] == nil then
-			message[count] = "" -- stops the expected string?
-			showMessage = false
-		end
-		if msgType == "box" then
-		    love.graphics.setColor( msgBoxBg )
-		    love.graphics.rectangle("fill", msgBoxPosX, msgBoxPosY, width-(padding*2), -(boxHeight) )
-		    love.graphics.setColor( msgFontColor )
-			love.graphics.print(message[count], msgBoxPosX+padding, msgBoxPosY-boxHeight+padding)
-		else
-		    love.graphics.setColor( msgFontColor )
-		    love.graphics.print(message, x, y )
-		end
+function love.update(dt)
+	sayIt.Update()
+	if love.keyboard.isDown('q') then
+		sayIt.New("Mike Pence", {"Take it up the crapper...", "Get the zapper!", "Traps are qt.", "\n"})
 	end
 end
 
-function sayIt_Update(dt)
-
+function love.draw( )
+	love.graphics.setBackgroundColor( 100, 100, 100 )
+	love.graphics.print(currentMessage, 10, 10)
+	sayIt.Draw()
 end
 
-function sayIt_Init()
-	count = 1
-	showMessage = true
+function love.keyreleased(key)
+	sayIt.Handler(key)
+	if key == "w" then
+		sayIt.New("Mike Pence", {"Take it up the crapper...", "Get the zapper!", "Traps are qt.", "\n"})
+		sayIt.New("Scotch Egg", {"Say...", "You wouldn't have happen to come across any", "Female cohorts whom art being taught?", "\n" })
+		sayIt.New("Bill Nye", {"This next segment coming up is really good", "Lots of schmear", "Oy vey!"})
+	end
 end
 
-function sayIt_Handler(key)
-   if key == "return" then
-      count = count + 1
-   end
+function love.quit( )
+	for k,v in pairs(messages) do print(k,v) end
+	for k,v in pairs(titles) do print(k,v) end
 end
---
+++ sayIt.lua
@@ -0,0 +1,65 @@
+-- A really crappy lua messenger
+sayIt = {}
+function sayIt.Init()
+	messages = {}
+	titles = {}
+	currentMessage = 1
+	currentTitle = 1
+	showingMessage = false
+end
+
+function sayIt.New(title, message)
+	table.insert(titles, title)
+	messages.title = titles[1]
+	-- Insert our messages into the table to be parsed
+	for k,v in ipairs(message) do
+		table.insert(messages, message[k])
+	end -- Let them be displayed
+	showingMessage = true
+end
+
+function sayIt.Update()
+	-- If all messages have been shown, close the msgbox.
+	if currentMessage > #messages then
+		showingMessage = false -- Hide the messages
+		currentMessage, currentTitle = 1, 1 -- Reset the counter position
+		-- Remove the shown messages / titles
+		for k,v in pairs(messages) do messages[k] = nil end
+		for k,v in pairs(titles) do titles[k] = nil end
+	end
+	if messages[currentMessage] == "\n" then -- End of sayIt.New
+		if messages[currentMessage+1] ~= nil then -- Allow "\n" on single message
+			-- Skip the "\n" msg, go to next title and disply it
+			currentMessage = currentMessage + 1
+			currentTitle = currentTitle + 1
+			messages.title = titles[currentTitle]
+		else -- Hide the "\n" whitespace (hacky :P)
+			showingMessage = false
+		end
+	end
+end
+
+function sayIt.Draw()
+	local width = love.graphics.getWidth()
+	local height = love.graphics.getHeight()
+	local padding = 10
+	local boxHeight = 150
+	local msgBoxPosX = 0+padding
+	local msgBoxPosY = height-padding
+	local msgBoxBg = { 255, 255, 255, 255 } -- RGBA
+	local msgFontColor = { 0, 0, 0, 255}
+	if showingMessage then
+	    love.graphics.setColor( msgBoxBg )
+	    love.graphics.rectangle("fill", msgBoxPosX, msgBoxPosY, width-(padding*2), -(boxHeight) )
+	    love.graphics.setColor( msgFontColor )
+	    love.graphics.print(messages.title, msgBoxPosX+padding, msgBoxPosY-boxHeight+(padding))
+		love.graphics.print(messages[currentMessage], msgBoxPosX+padding, msgBoxPosY-boxHeight+(padding*3))
+	end
+end
+
+function sayIt.Handler(key)
+	if key == "return" and showingMessage then
+		-- Show the next message in the array
+		currentMessage = currentMessage + 1
+	end
+end
