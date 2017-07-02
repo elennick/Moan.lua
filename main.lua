219a6bbbf156931ba765854175ee4a98fff6d8d1
twentytwoo <may.tt@protonmail.ch>
2017-07-02 16:55:50 +0100

    Typing effects, scaling images all done

--- main.lua
+++ main.lua
@@ -1,33 +1,56 @@
 require('sayIt')
+Camera = require "camera"
+
 
 function love.load()
 	sayIt.Init()
+	defaultfont = love.graphics.getFont()
+	p1 = { x = 100, y = 250 }
+	p2 = { x = 600, y = 250 }
+    camera = Camera(p1.x, p1.y, 0.5)
 end
 
 function love.update(dt)
+
 	sayIt.Update(dt)
 	if love.keyboard.isDown('q') then
-		sayIt.New("Mike Pence", {"Take it up the crapper...", "Get the zapper!", "Traps are qt."})
+		sayIt.New("Mike Pence", {"This is a message", "another one.", "Finally..."})
 	end
 end
 
 function love.draw(dt)
-    love.graphics.setColor( 255, 255, 255 )
-	love.graphics.setBackgroundColor( 100, 100, 100 )
-	love.graphics.print("Current queue: " .. currentMessage, 10, 10)
-	love.graphics.print("To print: " .. textToPrint, 10, 25)
-	love.graphics.print("Typing?: " .. tostring(typing), 10, 40)
+    camera:attach()
+		love.graphics.rectangle("fill", p1.x, p1.y, 50, 50 )
+		love.graphics.rectangle("fill", p2.x, p2.y, 50, 50 )
+    camera:detach()
+
+    love.graphics.push()
+    	love.graphics.setFont(defaultfont)
+	    love.graphics.setColor( 255, 255, 255 )
+		love.graphics.setBackgroundColor( 100, 100, 100 )
+		love.graphics.print("Current queue: " .. currentMessage .. "\n" ..
+							"To print: " .. textToPrint 		.. "\n" ..
+							"No. messages: " .. #messages 		.. "\n" ..
+							"Typing?: " .. tostring(typing) 	.. "\n" -- END
+							, 10, 10)
+	love.graphics.pop()
+
+
 	sayIt.Draw()
 end
 
 function love.keyreleased(key)
 	sayIt.Handler(key)
 	if key == "w" then
-		sayIt.New("Mike Pence", {"Take it up the crapper...", "Get the zapper!", "Traps are qt."})
+		sayIt.New("Mike Pence", {"This is a message", "another one.", "Finally...", "\n"}, p2.x, p2.y)
+		sayIt.New("Bill Nye", {"I love this next segment...", "Lots of schmear"}, p1.x, p1.y)
+	end
+	if key == "q" then
+		love.quit()
 	end
 end
 
-function love.quit( )
+function love.quit()
 	for k,v in pairs(messages) do print(k,v) end
 	for k,v in pairs(titles) do print(k,v) end
 end
--- sayIt.lua
+++ sayIt.lua
@@ -1,6 +1,9 @@
--- A really crappy lua messenger
+-- A really crappy hacky love2d dialogue thing
+-- github.com/twentytwoo
 sayIt = {}
 function sayIt.Init()
+	sayIt.Font = love.graphics.newFont("monobit.ttf", 32)
+	love.graphics.setDefaultFilter("nearest", "nearest") -- No AA
 	messages = {}
 	titles = {}
 	currentMessage = 1
@@ -17,9 +20,12 @@ function sayIt.Init()
 	typePosition = 0
 end
 
-function sayIt.New(title, message)
+function sayIt.New(title, message, x, y)
+	-- Let the first message have the first title
 	table.insert(titles, title)
 	messages.title = titles[1]
+	-- Set up the images, x, y
+	sayIt.Next()
 	-- Insert our messages into the table to be parsed
 	for k,v in ipairs(message) do
 		table.insert(messages, message[k])
@@ -29,6 +35,8 @@ function sayIt.New(title, message)
 end
 
 function sayIt.Update(dt)
+	collectgarbage() -- Somewhat helps things with below newImage
+
 	if messages[currentMessage] == "\n" then -- End of sayIt.New (multiple)
 		if messages[currentMessage + 1] ~= nil then -- Allow "\n" on single message
 			-- Skip the "\n" msg, go to next title and dispaly it
@@ -36,10 +44,14 @@ function sayIt.Update(dt)
 			currentTitle = currentTitle + 1
 			textToPrint = messages[currentMessage]
 			messages.title = titles[currentTitle]
+			titleImage = messages.title
+			sayIt.Next()
 		else -- Hide the "\n" whitespace (hacky :P)
 			showingMessage = false
 		end
 	end
+
+    --camera:lookAt(p2.x, p2.y)
 
 	if messages[currentMessage] == printedText then -- We've finished typing the sentence
 		typing = false
@@ -58,8 +70,7 @@ function sayIt.Update(dt)
 		    if typeTimer <= 0 then
 		        typeTimer = 0.05
 		        typePosition = typePosition + 1
-
-		        printedText = string.sub(textToPrint,0,typePosition)
+		        printedText = string.sub(textToPrint, 0, typePosition)
 		    end
 	    end
 	end
@@ -69,33 +80,45 @@ function sayIt.Draw(dt)
 	local width = love.graphics.getWidth()
 	local height = love.graphics.getHeight()
 	local padding = 10
-	local boxHeight = 150
+	local boxHeight = 125
+	local scale = 0.25
+	-- img dimensions = boxHeight - (2*padding)
 	local msgBoxPosX = 0+padding
 	local msgBoxPosY = height-padding
-	local msgBoxBg = { 255, 255, 255, 255 } -- RGBA
-	local msgFontColor = { 0, 0, 0, 255}
-	if showingMessage then
+	local msgBoxBg = { 0, 0, 0, 255 } -- RGBA
+	local msgFontColor = { 255, 255, 255, 255}
+	if showingMessage then -- Draw the message
+		-- Replace spaces in title with _ for the image
 	    love.graphics.setColor( msgBoxBg )
-	    love.graphics.rectangle("fill", msgBoxPosX, msgBoxPosY, width-(padding*2), -(boxHeight) )
-	    love.graphics.setColor( msgFontColor )
-	    love.graphics.print(messages.title, msgBoxPosX+padding, msgBoxPosY-boxHeight+(padding))
-		love.graphics.printf(printedText, msgBoxPosX+padding, msgBoxPosY-boxHeight+(padding*3), msgBoxPosX+width-(4*padding), "left")
-		if messages[currentMessage+1] ~= nil then
-			love.graphics.print(">", msgBoxPosX+width-(4*padding), msgBoxPosY-(2*padding))
-		end
+	    love.graphics.rectangle("fill", msgBoxPosX, msgBoxPosY, width-(padding*2), -(boxHeight), 10, 0, 10 )
+
+	    love.graphics.push()
+	    	love.graphics.scale(scale, scale)
+	    	love.graphics.setColor(255,255,255)
+			love.graphics.draw(titleImage, (msgBoxPosX+padding)*(1/scale), msgBoxPosY*(1/scale)-titleImgHeight-padding*(1/scale)) -- Look into a better way of this -> collectgarbage()
+	    love.graphics.pop()
+
+	    love.graphics.push()
+	    	love.graphics.setFont(sayIt.Font)
+		    love.graphics.setColor( msgFontColor )
+		    love.graphics.print(messages.title, msgBoxPosX+(2*padding)+(titleImgWidth/(1/scale)), msgBoxPosY-boxHeight)
+			love.graphics.printf(printedText, msgBoxPosX+(2*padding)+(titleImgWidth/(1/scale)), msgBoxPosY-boxHeight+(padding*2), msgBoxPosX+width-(4*padding), "left")
+			if messages[currentMessage+1] ~= nil then
+				love.graphics.print(">", msgBoxPosX+width-(4*padding), msgBoxPosY-(3.5*padding))
+			end
+		love.graphics.pop()
 	end
 end
 
 function sayIt.Handler(key)
 	if key == "return" and showingMessage then
 		if typing == true then -- We can skip the typing
-			-- Make the output == to message
 			printedText = messages[currentMessage]
 			-- Tell it we've finished typing
 			typePosition = string.len(messages[currentMessage])
 		else -- We can show a new message!
-			-- Check the current message queue
-			if messages[currentMessage+1] == nil then -- Close everything
+			-- Check the current message queue if we've finsihed
+			if messages[currentMessage + 1] == nil then -- Close everything
 				textToPrint = "" -- Do not crash - please!
 				showingMessage = false
 				currentMessage, currentTitle = 1, 1 -- Reset the counter position
@@ -110,4 +133,9 @@ function sayIt.Handler(key)
 			end
 		end
 	end
+end
+
+function sayIt.Next()
+	titleImage = love.graphics.newImage(string.gsub(messages.title, " ", "_") .. ".png")
+	titleImgWidth, titleImgHeight = titleImage:getWidth(), titleImage:getHeight()
 end
